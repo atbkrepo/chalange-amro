cece991ebeef0071360cb489efb75641b01e5e75
oleksii <4oleksii.k@gmail.com>
2026-03-26 09:53:50 +0100

    fix context load for config server

--- config/src/test/java/com/xyz/config/ConfigApplicationTests.java
+++ config/src/test/java/com/xyz/config/ConfigApplicationTests.java
@@ -2,8 +2,13 @@ package com.xyz.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.profiles.active=native",
+        "spring.cloud.config.server.native.searchLocations=classpath:/config"
+})
 class ConfigApplicationTests {
 
 	@Test
